844683fede4034a86067bead7006df631a1712f5
xuwei-k <6b656e6a69@gmail.com>
2020-10-31 03:41:01 +0900

    throw error if RoutesCompiler generate duplicate routes files

---
+++ dev-mode/sbt-plugin/src/sbt-test/play-sbt-plugin/routes-compiler-issue-8423/app/controllers/sample/Controller2.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package controllers.sample;
+
+import play.mvc.*;
+
+public class Controller2 extends Controller {
+
+    public Result index() {
+        return null;
+    }
+
+}
---
+++ dev-mode/sbt-plugin/src/sbt-test/play-sbt-plugin/routes-compiler-issue-8423/build.sbt
@@ -0,0 +1,6 @@
+//
+// Copyright (C) Lightbend Inc. <https://www.lightbend.com>
+//
+enablePlugins(PlayJava)
+
+scalaVersion := "2.13.3"
